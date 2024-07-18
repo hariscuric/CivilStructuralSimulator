9f91fe4aa3993c0dd9c15638278956c4e88f1ccc
hariscuric <hariscuric123@gmail.com>
2024-07-18 15:45:09 +0200

    Project preliminary planning

---
+++ main.py
@@ -0,0 +1,27 @@
+import graphics as gr
+
+
+def main():
+
+    print("This is structural analysis program")
+
+    #Input structural geometry
+        #input frame element start-end point coordinates
+            #list and enumerate the nodes based on start-end point coordinates of elements
+            #tie elements to nodes
+        #specify element properties
+            #specify materials
+            #specify cross section
+                #compute section moduli
+                #build element stiffness matrix
+                #transform element stiffness matrix to global coordiates
+        #Compute global structural stiffness matrix
+
+    #Visualize the geometry
+
+    #Input the loads
+
+    #Compute the responses
+    
+
+main()
