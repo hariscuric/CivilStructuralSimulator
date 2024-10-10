c3feedbb38f3a08be7c1d65abca60c9ac14928ab
hariscuric <hariscuric123@gmail.com>
2024-10-10 15:30:38 +0200

    Minor Bug fixed

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ def main():
 
     elements = inp.inputElements('input.txt')
     structure = sss.structure(elements)
+    print(structure.elements[1].localForces)
+    exit()
 
 
     camera = pP.camera(v.vector3(10,10,10),v.vector3(-1,-1,-1).normalize())
